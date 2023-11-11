7be2227bd5a9265cd726b9724b2e5d8e7df400c7
Rishabh Verma <rishabh.v@clappia.com>
2023-11-11 15:40:24 +0530

    Lesson 1 about useState

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Linking, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name,setName] = useState('Rahul')
+  const [session,setSession] = useState({
+    number:0,
+    title:'React Native'  
+  })
+  const [value,setalue] = useState(true)
+
+  const clickHanlder = () => {
+   setName('Rahul Kumar')
+    setSession({
+      number:1,
+      title:'React Native Session 1'
+    })
+    setalue((prev) => !prev)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}> {name}</Text>
+      <Text style={styles.text}> {session.number}</Text>
+      <Text style={styles.text}>
+        {value ? 'True' : 'False'}
+      </Text>
+      <Button title="Click me to chnage name" onPress={clickHanlder} />
     </View>
   );
 }
@@ -13,8 +33,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color: 'red',
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
 });
