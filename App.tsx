d172bc420b8f85fe49f6ede7dd2c2fed58a27c90
Rishabh Verma <rishabh.v@clappia.com>
2023-11-11 16:18:05 +0530

    Lesson 4 on Map

--- App.tsx
+++ App.tsx
@@ -1,21 +1,58 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, Linking, StyleSheet, Text, View } from "react-native";
+import { Button, Linking, RefreshControl, ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("John Doe");
+  const [refreshing, setRefreshing] = useState(false);
+  const [items, setItems] = useState([
+    { id: 1, text: "Item 1" },
+    { id: 2, text: "Item 2" },
+    { id: 3, text: "Item 3" },
+    { id: 4, text: "Item 4" },
+    { id: 5, text: "Item 5" },
+    { id: 6, text: "Item 6" },
+    { id: 7, text: "Item 7" },
+    { id: 8, text: "Item 8" },
+    { id: 9, text: "Item 9" },
+    { id: 10, text: "Item 10" },
+    { id: 11, text: "Item 11" },
+    { id: 12, text: "Item 12" },
+  ]);
   return (
     <>
       <View style={styles.body}>
-        <View style={styles.view1}>
-          <Text>{name}</Text>
+        {/* <ScrollView horizontal> */}
+        <ScrollView
+        
+        refreshControl={
+          <RefreshControl
+          colors={["red", "green", "blue"]}
+          refreshing={refreshing}
+          onRefresh={() => {
+            setRefreshing(true);
+            setTimeout(() => {
+              setRefreshing(false);
+            }, 2000);
+          }}
+          />
+        }>
+        {items.map((item) => (
+          <View key={item.id} style={styles.item}>
+            <Text>{item.text}</Text>
+          </View>
+        ))}
+        </ScrollView>
+        {/* <View style={styles.item}>
+          <Text>1</Text>
         </View>
-        <View style={styles.view2}>
-          <Text>{name}</Text>
+        <View style={styles.item}>
+          <Text>2</Text>
         </View>
-        <View style={styles.view3}>
-          <Text>{name}</Text>
-        </View>
+        <View style={styles.item}>
+          <Text>3</Text>
+        </View> */}
+
+        
       </View>
     </>
   );
@@ -23,41 +60,15 @@ export default function App() {
 
 const styles = StyleSheet.create({
   body: {
-    marginTop: 50,
+    flex: 1,
+    marginTop: 30,
+    flexDirection: "column",
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "space-between",
-    // height: "80%",
-    // width: "80%",
-    flex: 1,
-    // flexDirection: 'column',
-    flexDirection: "row",
   },
-  view1: {
-    // width: 100,
-    // height: 100,
-    flex: 1,
-    backgroundColor: "red",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    borderRadius: 50,
-  },
-  view2: {
-    flex: 3,
-    // width: 100,
-    // height: 100,
-    backgroundColor: "blue",
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 50,
-  },
-  view3: {
-    flex: 2,
-    // width: 100,
-    // height: 100,
-    backgroundColor: "green",
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 50,
+  item: {
+    backgroundColor: "pink",
+    padding: 30,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
 });
