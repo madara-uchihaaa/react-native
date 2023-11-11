8bcc708e6ae02217045d9bb3417230b91c314bba
Rishabh Verma <rishabh.v@clappia.com>
2023-11-11 17:05:12 +0530

    Lesson 6 on Text Input

--- App.tsx
+++ App.tsx
@@ -1,68 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, FlatList, Linking, RefreshControl, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  Linking,
+  RefreshControl,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
-  const [refreshing, setRefreshing] = useState(false);
-  const [items, setItems] = useState([
-    { id: 1, text: "Item 1" },
-    { id: 2, text: "Item 2" },
-    { id: 3, text: "Item 3" },
-    { id: 4, text: "Item 4" },
-    { id: 5, text: "Item 5" },
-    { id: 6, text: "Item 6" },
-    { id: 7, text: "Item 7" },
-    { id: 8, text: "Item 8" },
-    { id: 9, text: "Item 9" },
-    { id: 10, text: "Item 10" },
-    { id: 11, text: "Item 11" },
-    { id: 12, text: "Item 12" },
-  ]);
+  const [name, setName] = useState("");
   return (
     <>
-      {/* <View style={styles.body}> */}
-        {/* <ScrollView horizontal> */}
-        {/* <ScrollView
-        
-        refreshControl={
-          <RefreshControl
-          colors={["red", "green", "blue"]}
-          refreshing={refreshing}
-          onRefresh={() => {
-            setRefreshing(true);
-            setTimeout(() => {
-              setRefreshing(false);
-            }, 2000);
-          }}
-          />
-        }>
-        {items.map((item) => (
-          <View key={item.id} style={styles.item}>
-            <Text>{item.text}</Text>
-          </View>
-        ))}
-        </ScrollView> */}
-        {/* <View style={styles.item}>
-          <Text>1</Text>
-        </View>
-        <View style={styles.item}>
-          <Text>2</Text>
-        </View>
-        <View style={styles.item}>
-          <Text>3</Text>
-        </View> */}
-
-        
-      {/* </View> */}
-      <FlatList 
-      data={items}
-      renderItem={({item}) => (
-        <View style={styles.item}>
-          <Text>{item.text} {item.id}</Text>
-        </View>
-      )}
-      />
-
+      <View style={styles.body}>
+        <Text style={styles.text}>Please enter your name</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your name"
+          onChangeText={(val) => setName(val)}
+          multiline
+          secureTextEntry={true}
+        />
+        <Text style={styles.text}>Hello {name}</Text>
+      </View>
     </>
   );
 }
@@ -72,17 +36,25 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 30,
     flexDirection: "column",
+    alignItems: "center",
     backgroundColor: "#fff",
   },
-  item: {
-    backgroundColor: "pink",
-    padding: 30,
-    marginVertical: 8,
-    marginHorizontal: 16,
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "red",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 10,
+    margin: 10,
+    textAlign: "center",
+    width: 300,
+  },
+  button: {
+    backgroundColor: "red",
+    padding: 10,
+    margin: 10,
   },
 });
-// Similar to FlatList therse is also a SectionList
-// Different between FlatList and SectionList is that SectionList is used to display data in sections
-// and FlatList is used to display data in a list
-// SectionList is used to display data in sections
-// FlatList is used to display data in a list
